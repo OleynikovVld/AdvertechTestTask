13869b2a124565429c00634a50ceec45f6a86586
OleynikovVld <oleynikov.vld@gmail.com>
2023-07-06 12:56:59 +0300

    Validation refactor

--- src/lib/bloc/contact_bloc.dart
+++ src/lib/bloc/contact_bloc.dart
@@ -5,17 +5,16 @@ part 'contact_event.dart';
 part 'contact_state.dart';
 
 class ContactBloc extends Bloc<ContactEvent, ContactState> {
-  ContactBloc() : super(ContactInitial()) {
-    on<CheckIfValidEvent>((event, emit) {
-      if (event.name == '') {
-        emit(ContactInvalidState());
-      } else if (event.email == '' || !EmailValidator.validate(event.email)) {
-        emit(ContactInvalidState());
-      } else if (event.message == '') {
-        emit(ContactInvalidState());
-      } else {
-        emit(ContactValidState());
-      }
-    });
+  ContactBloc() : super(ContactInvalidState()) {
+    on<CheckIfValidEvent>(_checkIfValid);
+  }
+
+  _checkIfValid(CheckIfValidEvent event, Emitter<ContactState> emit) {
+    if (event.name != '' &&
+        event.email != '' &&
+        event.message != '' &&
+        EmailValidator.validate(event.email)) {
+      emit(ContactValidState());
+    }
   }
 }
